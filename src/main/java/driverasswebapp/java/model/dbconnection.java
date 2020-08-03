013b0c49793d416c331f7fa1b6437a1367e4441e
Ammar_Fahmy <40536975+AmmarFahmy@users.noreply.github.com>
2020-08-03 17:23:52 +0530

    Database connection provided

---
+++ src/main/java/driverasswebapp/java/model/dbconnection.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package driverasswebapp.java.model;
+
+import java.sql.*;
+/**
+ *
+ * @author ammar
+ */
+public class dbconnection {
+    
+    private static Connection con;
+    
+    public static Connection getConnection(){
+        
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            //con=DriverManager.getConnection("jdbc:mysql://localhost:3306/driverapp","root","Spencer.nk375");
+            con=DriverManager.getConnection("jdbc:mysql://localhost:3306/driverapp","","");
+            
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        return con;
+    }
+    
+}
